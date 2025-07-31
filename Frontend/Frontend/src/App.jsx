84ba33d6f09a921e64e5a3f5e7e36e80ec54963a
Jeels <jeelsherathiya25@gmail.com>
2025-07-31 15:59:50 +0530

    all

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -2,16 +2,13 @@ import { Routes , Route} from 'react-router-dom'
 import Login from './Componenets/Login'
 import Verify from './Componenets/Verify'
 import Welcome from './Componenets/Welcome'
-<<<<<<< HEAD
 import Header from './Componenets/Header'
 import Address from './Componenets/Address'
 import ContactUs from './Componenets/ContactUs'
 import TermsAndConditions from './Componenets/TermAndConditions'
 import TrackOrder from './Componenets/TrackOrder'
 import OrderHistory from './Componenets/OrderHistory'
-=======
 import Home from './Componenets/Home'
->>>>>>> d1bbe2f565997decc142d8146724e2ee6ce7cc4f
 
 function App() {
 
@@ -24,17 +21,15 @@ function App() {
       <TermsAndConditions/>
       <Address/>
        <Routes>
-<<<<<<< HEAD
+
       <Route path="/" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
       <Route path="/verify" element={<Verify />} />
       
-=======
       <Route exact path="/" element={<Welcome />} />
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/verify" element={<Verify />} />
       <Route exact path="/home" element={<Home />} />
->>>>>>> d1bbe2f565997decc142d8146724e2ee6ce7cc4f
     </Routes>
     </>
   )
